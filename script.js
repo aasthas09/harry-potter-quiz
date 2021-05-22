f501d797d6007df928be1487aa13a88e98013ad2
aasthas09 <84279637+aasthas09@users.noreply.github.com>
2021-05-22 21:17:55 +0530

    Changes Made

---
+++ script.js
@@ -0,0 +1,190 @@
+function handleStartQuiz() {
+  $('.nextQuestion').on('click', function() {
+    $('header').hide();
+    $('form').show();
+    $('#container').show();
+    $('footer').show();
+    $('.questionAnswerForm').hide();
+    console.log('startQuiz is working');
+  });
+}
+
+let questionNumber = 1;
+let score = 0;
+
+function generateQuestion() {
+  iterateQuestion();
+  iterateScore();
+  $('.results').hide();
+  return `<section class="questionPage" role="Question Page">
+    <h2 class="question">${STORE[questionNumber - 1].question}</h2>
+      <form role="form" accept-charset="UTF-8">
+        <fieldset>
+          <label>
+            <input type="radio" class="answer" name="option" value="${
+              STORE[questionNumber - 1].answer1
+            }"/>
+            ${STORE[questionNumber - 1].answer1}<br>
+          </label>
+          <label>
+            <input type="radio" class="answer" name="option" value="${
+              STORE[questionNumber - 1].answer2
+            }"/>
+            ${STORE[questionNumber - 1].answer2}<br>
+          </label>
+          <label>
+            <input type="radio" class="answer" name="option" value="${
+              STORE[questionNumber - 1].answer3
+            }"/>
+            ${STORE[questionNumber - 1].answer3}<br>
+          </label>
+          <label>
+            <input type="radio" class="answer" name="option" value="${
+              STORE[questionNumber - 1].answer4
+            }"/>
+            ${STORE[questionNumber - 1].answer4}<br>
+          </label>
+        </fieldset>
+      </form>
+      <button type="submit" class="getResults">Submit</button>
+      </section>
+    `;
+  console.log('generateQuestion is working');
+}
+
+function renderQuestion() {
+  console.log('renderQuestion question number', questionNumber);
+  $('#container').html(generateQuestion());
+  $('header').hide();
+  $('form').show();
+  $('#container').show();
+  $('.questionAnswerForm').hide();
+}
+
+function handleNextQuestion() {
+  $('.nextQuestion').on('click', function() {
+    if (questionNumber <= STORE.length) {
+      renderQuestion();
+      questionNumber++;
+    } else {
+      renderResults();
+    }
+    console.log('handleNextQuestion is working');
+  });
+}
+
+function handleAnswerSubmitted() {
+  $('#container').on('click', '.getResults', function() {
+    //event.preventDefault();
+    checkUserAnswer();
+    $('.questionAnswerForm').show();
+  });
+}
+
+function checkUserAnswer() {
+  event.preventDefault();
+  //Need to fix this so there's no -2 value
+  let correctAnswer = `${STORE[questionNumber - 2].correctAnswer}`;
+  let userAnswer = $('input:checked').val();
+
+  if (userAnswer == correctAnswer) {
+    console.log('checkUserAnswer score', score);
+    correctAnswerFeedback();
+  } else {
+    console.log('incorrectAnswer selected');
+    incorrectAnswerFeedback();
+  }
+}
+
+function correctAnswerFeedback() {
+  score++;
+  iterateScore();
+  $('header').hide();
+  $('form').hide();
+  $('#container').hide();
+  $('footer').show();
+  $('.questionAnswerForm').html(`<div role="banner" class="results">
+  <iframe src="https://giphy.com/embed/26gN16cJ6gy4LzZSw" width="480" height="270" frameBorder="0" class="giphy-embed" alt="10 Points For Gryffindor" allowFullScreen></iframe>  
+  
+    <button type="submit" class="nextQuestion">Next Question</button>
+    </div>`);
+  if (questionNumber <= STORE.length) {
+    handleNextQuestion();
+  } else {
+    renderResults();
+  }
+}
+
+function incorrectAnswerFeedback() {
+  iterateScore();
+  $('header').hide();
+  $('form').hide();
+  $('#container').hide();
+  $('footer').show();
+  $('.questionAnswerForm').html(`<div role="banner" class="results">
+    <h2>Nope, The Correct Answer Is ${
+      STORE[questionNumber - 2].correctAnswer
+    }!</h2>
+    
+    <iframe src="https://giphy.com/embed/v1xj1xo2zHz1u" width="480" height="270" frameBorder="0" class="giphy-embed" alt="Try Again" allowFullScreen></iframe>
+    <button type="submit" class="nextQuestion">Next Question</button>
+    </div>`);
+  if (questionNumber <= STORE.length) {
+    handleNextQuestion();
+  } else {
+    renderResults();
+  }
+}
+
+function iterateQuestion() {
+  $('.currentQuestion').html(`<p>Question: ${questionNumber} / 10</p>`);
+}
+
+function iterateScore() {
+  let houseScore = score * 10;
+  $('.currentScore').html(`<p>House Points Earned: ${houseScore}</p>`);
+}
+
+function generateResults() {
+  if (score >= 8) {
+    return $('.resultsPage').html(`<header role="banner">
+        <h1 class="resultsHeader">You Are The Chosen One!</h1>
+        <p><iframe src="https://giphy.com/embed/9gc43QEEmxSMM" width="480" height="247" frameBorder="0" class="giphy" alt="Hogwarts Celebration" allowFullScreen></iframe></p>
+        
+        <h2>You Got ${score} Out Of 10 Correct</h2>
+        <button type="submit" class="restartQuiz">Restart Quiz</button>
+      </header >`);
+  } else {
+    return $('.resultsPage')
+      .html(`<header role="banner"><h1 class="resultsHeader">The Dark Lord Has Defeated You!</h1>
+      <p><iframe src="https://giphy.com/embed/LLxwPAjfpLak8" width="480" height="198" frameBorder="0" class="giphy" alt="Voldemort Avada Kedavra" allowFullScreen></iframe></p>
+      <h2>You Got ${score} Out Of 10 Correct</h2>
+      <button type="submit" class="restartQuiz">Restart Quiz</button>
+      </header >`);
+  }
+}
+
+function renderResults() {
+  $('.nextQuestion').on('click', function() {
+    $('.results').hide();
+    generateResults();
+    console.log('renderResults is working');
+    restartQuiz();
+  });
+}
+
+function restartQuiz() {
+  $('.restartQuiz').on('click', function() {
+    location.reload();
+    console.log('restartQuiz is working');
+  });
+}
+
+function handleQuiz() {
+  handleStartQuiz();
+  handleAnswerSubmitted();
+  handleNextQuestion();
+  restartQuiz();
+}
+
+$(handleQuiz);
---
+++ store.js
@@ -0,0 +1,98 @@
+const STORE = [
+  {
+    questionNumber: 1,
+    question: 'What position does Harry play on his Quidditch team?',
+    answer1: 'Chaser',
+    answer2: 'Seeker',
+    answer3: 'Bludger',
+    answer4: 'Keeper',
+    correctAnswer: 'Seeker'
+  },
+  {
+    questionNumber: 2,
+    question: 'Name the centaur who taught at Hogwarts.',
+    answer1: 'Professor Dumbledore',
+    answer2: 'James Potter',
+    answer3: 'Professor Firenze',
+    answer4: 'Professor McGonagall',
+    correctAnswer: 'Professor Firenze'
+  },
+  {
+    questionNumber: 3,
+    question: 'Cedric Diggory faced what breed of dragon in the Triwizard Tournament?',
+    answer1: 'Swedish Short-Snout',
+    answer2: 'Peruvian Vipertooth',
+    answer3: 'Norwegian Ridgeback',
+    answer4: 'Common Welsh Green',
+    correctAnswer: 'Swedish Short-Snout'
+  },
+  {
+    questionNumber: 4,
+    question:
+      'What animal represents Hufflepuff house?',
+    answer1: 'Lion',
+    answer2: 'Eagle',
+    answer3: 'Badger',
+    answer4: 'Serpent',
+    correctAnswer: 'Badger'
+  },
+  {
+    questionNumber: 5,
+    question: 'Where did Harry and Ron crash the Ford Anglia?',
+    answer1: 'Hogwarts',
+    answer2: 'Whomping Willow',
+    answer3: 'Forbidden Forest',
+    answer4: 'Hogsmeade',
+    correctAnswer: 'Whomping Willow'
+  },
+  {
+    questionNumber: 6,
+    question:
+      'In the Hall of Prophecy there are rows and rows of glowing orbs. Which row contains the prophecy about Harry and Voldemort?',
+    answer1: "Row 97",
+    answer2: 'Row 20',
+    answer3: 'Row 74',
+    answer4: 'Row 89',
+    correctAnswer: 'Row 97'
+  },
+  {
+    questionNumber: 7,
+    question:
+      'Which character served as a Quidditch commentator?',
+    answer1: 'Lee Jordan' , 
+    answer2: 'George Weasley',
+    answer3: ' Ron Weasley',
+    answer4: 'Oliver Wood',
+    correctAnswer: 'Lee Jordan'
+  },
+  {
+    questionNumber: 8,
+    question:
+      'Luna tells Harry ‘things we lose have a way of coming back to us in the end. If not always in the ways we expect’ but who used to say this to Luna?',
+    answer1: 'Her Mum',
+    answer2: 'Nargles',
+    answer3: 'Her Dad',
+    answer4: 'Her Grandmother',
+    correctAnswer: 'Her Mum'
+  },
+  {
+    questionNumber: 9,
+    question:
+      'Gilderoy Lockhart tried to use ‘Brackium Emendo’ to fix Harry’s broken bones. What did it actually do to him?', 
+    answer1: 'Turned his leg wooden',
+    answer2:' Removed his bones entirely',
+    answer3: 'Forced him to speak Parseltongue',
+    answer4: 'Gave him an exquisite singing voice',
+    correctAnswer: ' Removed his bones entirely'
+  },
+  {
+    questionNumber: 10,
+    question:
+      'How many staircases does Hogwarts have?',
+    answer1: "149",
+    answer2: '264',
+    answer3: '387',
+    answer4: '142',
+    correctAnswer: '142'
+  }
+];
